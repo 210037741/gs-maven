0aa0529dbce9c0b816314ecaefe8d14e81f6f5be
CPUT Student <stud@ubuntu.(none)>
2014-02-18 06:04:30 -0800

    This is so stupidgit commit -mgit commit -m Bleh

--- initial/src/main/java/hello/HelloWorld.java
+++ initial/src/main/java/hello/HelloWorld.java
@@ -1,5 +1,5 @@
 package hello;
-
+/*This is the most easiest but difficult...if that makes sense*/
 public class HelloWorld {
     public static void main(String[] args) {
         Greeter greeter = new Greeter();
